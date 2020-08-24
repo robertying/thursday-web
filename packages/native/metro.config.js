505e01b6b388ee898169b5f45897ae5560f41dca
Rui Ying <yingrui205@gmail.com>
2020-08-24 18:46:24 +0800

    Setup for monorepo & add eslint for each

--- packages/native/metro.config.js
+++ packages/native/metro.config.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 module.exports = {
+  watchFolders: [path.resolve(__dirname, '../../node_modules')],
   transformer: {
     assetPlugins: ['expo-asset/tools/hashAssetFiles'],
   },
