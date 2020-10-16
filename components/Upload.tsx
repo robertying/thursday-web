70c991b3c36b52d6fa539b9638f5cd7eb92389d6
Rui Ying <yingrui205@gmail.com>
2020-10-16 09:53:41 +0800

    Remove alt for upload placeholder image

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -166,7 +166,7 @@ const Upload: React.FC<UploadProps> = ({
             ref={imgRef}
             className={`${classes.image} ${classes.loading}`}
             style={{ cursor: "pointer" }}
-            alt="上传图片"
+            alt=""
           />
           {loading && <CircularProgress className={classes.loading} />}
         </label>
